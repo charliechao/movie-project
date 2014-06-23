2509efdfcbfbbd9613f6da28525e929d8016c488
unknown <charlie@gocharlie.co.nz>
2014-06-23 20:35:56 +1200

    edited footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,10 @@
  <footer class="navbar-fixed-bottom">
     <div class="container-fluid">                  
    <div class="row" id="footer">
-     <div class="col-xs-4 col-xs-offset-4 text-center" id="footer-text"><a>Copy Rights</a> | <a>Terms and Conditions</a> | <a>Company</a></div>
+     <div class="col-xs-4 col-xs-offset-4 text-center" id="footer-text">
+     <a href="http://www.copyright.gov/" target="_blank">Copy Rights</a> | 
+     <a href="http://en.wikipedia.org/wiki/Contractual_term" target="_blank">Terms and Conditions</a> | 
+     <a href="http://www.paramountpictures.co.nz/" target="_blank">Company</a></div>
    
  
  <div class="col-xs-4 pull-right">  <p>© 2013 Paramount Pictures. All Rights Reserved.<img src="img/paramount_logo.png" id="logo" /></p> 
